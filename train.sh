071c019a17a0a7c3e7aa021f28c13e78b4592746
Martín Dutra <martindsq@hotmail.com>
2026-05-28 18:18:25 -0300

    update train regime

--- train.sh
+++ train.sh
@@ -2,5 +2,8 @@
 
 MAIL_USER=${USER_MAIL:?Missing USER_MAIL}
 
-sbatch --job-name="train-lr3e4-t45-e15-c100" --mail-user="$MAIL_USER" \
-  train.batch dual-route convolutional convolutional 256 256 100 45 3e-4 45 3e-5
+sbatch --job-name="train-phones-tmax60-lr3e4-t45-ctc01-c200" --mail-user="$MAIL_USER" \
+  train.batch dual-route convolutional convolutional 256 256 200 60 3e-4 45 3e-5 0.1 \
+  1 0 0 \
+  0.2 0.8 0 \
+  0 0 0
